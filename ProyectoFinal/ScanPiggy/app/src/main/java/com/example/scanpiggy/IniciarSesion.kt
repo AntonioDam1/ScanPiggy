bf3a1dc42d43be61c2eb1500830d4dd01619b699
AntonioDam2B <antonioaguidam1@gmail.com>
2024-04-08 01:00:47 +0200

    Primer Commit del Proyecto Final

---
+++ ProyectoFinal/ScanPiggy/app/src/main/java/com/example/scanpiggy/IniciarSesion.kt
@@ -0,0 +1,34 @@
+package com.example.scanpiggy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+
+class IniciarSesion : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_iniciar_sesion)
+
+        val gifImageView = findViewById<ImageView>(R.id.gifImageView)
+        Glide.with(this).load(R.drawable.huchaunscreen).into(gifImageView)
+
+        val imageGmail = findViewById<ImageView>(R.id.imageGmail)
+        imageGmail.setOnClickListener {
+            val intent = packageManager.getLaunchIntentForPackage("com.google.android.gm")
+            startActivity(intent)
+        }
+
+        val imageFacebook = findViewById<ImageView>(R.id.imageFacebook)
+        imageFacebook.setOnClickListener {
+            val intent = packageManager.getLaunchIntentForPackage("com.facebook.katana")
+            startActivity(intent)
+        }
+
+        val imageTwitter = findViewById<ImageView>(R.id.imageTwitter)
+        imageTwitter.setOnClickListener {
+            val intent = packageManager.getLaunchIntentForPackage("com.twitter.android")
+            startActivity(intent)
+        }
+    }
+}
---
+++ ProyectoFinal/ScanPiggy/app/src/main/java/com/example/scanpiggy/Loading.kt
@@ -0,0 +1,26 @@
+package com.example.scanpiggy
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+
+class Loading : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_loading)
+
+        val btnRegister = findViewById<Button>(R.id.btnRegister)
+        val btnLogin = findViewById<Button>(R.id.btnLogin)
+
+        btnRegister.setOnClickListener {
+            startActivity(Intent(this, Registrar::class.java))
+        }
+
+        btnLogin.setOnClickListener{
+            startActivity(Intent(this, IniciarSesion::class.java))
+        }
+    }
+}
